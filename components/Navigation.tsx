1b7df08fc9e0a93639771e2047b05bd7766f461e
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2022-10-11 14:15:13 +0300

    Fix navigation

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -6,37 +6,39 @@ import Link from "next/link";
 export function Navigation() {
   return (
     <nav className="flex flex-col justify-between p-4 w-64 border-r bg-gray-50 overflow-auto">
-      <div className="flex gap-3 items-center">
-        <div>
-          <Image
-            src="https://avatars.githubusercontent.com/u/4082806"
-            alt="Avatar"
-            width="48px"
-            height="48px"
-            className="rounded-xl"
-          />
+      <div>
+        <div className="flex gap-3 items-center mb-4">
+          <div>
+            <Image
+              src="https://avatars.githubusercontent.com/u/4082806"
+              alt="Avatar"
+              width="48px"
+              height="48px"
+              className="rounded-xl"
+            />
+          </div>
+          <div>
+            <p className="font-bold">Miro Rauhala</p>
+          </div>
         </div>
-        <div>
-          <p className="font-bold">Miro Rauhala</p>
-        </div>
-      </div>
 
-      <ul>
-        <li>
-          <Link href="/">
-            <a className="transition py-1 px-2 hover:bg-gray-200 rounded-lg inline-flex py-1 px-2 mb-1">
-              Home
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a className="transition py-1 px-2 hover:bg-gray-200 rounded-lg inline-flex py-1 px-2 bg-gray-200 rounded-lg">
-              Home
-            </a>
-          </Link>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <Link href="/">
+              <a className="transition py-1 px-2 hover:bg-gray-200 rounded-lg inline-flex mb-1">
+                Home
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/">
+              <a className="transition py-1 px-2 hover:bg-gray-200 rounded-lg inline-flex bg-gray-200">
+                Home
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
 
       <div className="flex flex-col gap-1">
         <ul className="flex gap-2">
