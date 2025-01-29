c0d976f8ec648d5e37bbc648b45306a5c9ac2178
Miro Rauhala <4082806+mirorauhala@users.noreply.github.com>
2025-01-29 20:19:10 +0200

    reword

--- app/work/page.tsx
+++ app/work/page.tsx
@@ -9,13 +9,13 @@ export default function Work() {
 
       <main className="mx-auto max-w-screen-2xl border-white/20 px-4 py-6 pt-24">
         <h2 className="mb-5 font-serif text-7xl font-bold tracking-tighter text-white">
-          Personal projects
+          My Work
         </h2>
 
         <p className="w-2/3 py-2 font-serif text-2xl text-white">
-          These are some of my projects. Most of my professional work is
-          confidential, unfortunately. I hope that these give you some idea of
-          my work.
+          Here are some of my projects that I've worked on. Most of my
+          professional work is confidential, unfortunately. I hope that these
+          give you some idea of what I do.
         </p>
 
         <Projects />
